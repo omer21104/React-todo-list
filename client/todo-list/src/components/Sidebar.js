2f0a2667d229b287aef8441542655af75342346c
omer <omer@DESKTOP-3P56U1G>
2022-02-02 10:11:25 +0200

    Changed sidebar to hold a dynamic list of todo-lists

--- client/todo-list/src/components/Sidebar.js
+++ client/todo-list/src/components/Sidebar.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useState } from "react";
+import Button from "@mui/material/Button";
 
 const Sidebar = () => {
-    return (
-        <div className={'sidebar'}>
-            <ul>
-                <li>le sidebar item</li>
-                <li>le sidebar item</li>
-                <li>le sidebar item</li>
-                <li>le sidebar item</li>
-            </ul>
-        </div>
-    );
+  const [lists, setLists] = useState(["list 1", "list 2", "list 3"]);
+
+  return (
+    <div className={"sidebar"}>
+      <h2>Your lists: </h2>
+      <ul>
+        {lists.map((list) => {
+          return <h2>{list}</h2>;
+        })}
+      </ul>
+      <Button variant={"contained"}>New list</Button>
+    </div>
+  );
 };
 
 export default Sidebar;
